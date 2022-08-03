be439618e10e5360ce9bbca8230b8812158f82f3
oezeb <eouedraogo4@gmail.com>
2022-08-03 12:29:44 +0800

    make it possible to call using command line

--- main.cc
+++ main.cc
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <string.h>
 #include "huffman.h"
 
-int main() {
-	huffman::compress("test/mississippi.txt", "test/out/mississippi.o");
-	huffman::decompress("test/out/mississippi.o", "test/out/mississippi.o.txt");
+using namespace std;
 
-	huffman::compress("test/lenna.jpg", "test/out/lenna.o");
-	huffman::decompress("test/out/lenna.o", "test/out/lenna.o.jpg");
+int main(int argc, char** argv) {
+	/**
+	 * commands:
+	 *    huffman -c <infile> <outfile>
+	 *    huffman -d <infile> <outfile>
+	 */
 
-    huffman::compress("test/civil_code.txt", "test/out/civil_code.o");
-	huffman::decompress("test/out/civil_code.o", "test/out/civil_code.o.txt");
-	
-    huffman::compress("test/sherlock_holmes.txt", "test/out/sherlock_holmes.o");
-	huffman::decompress("test/out/sherlock_holmes.o", "test/out/sherlock_holmes.o.txt");
+	if (argc == 4) {
+		if (strcmp(argv[1], "-c") == 0) {
+			huffman::compress(argv[2], argv[3]);
+			return 0;
+		} else if (strcmp(argv[1], "-d") == 0) {
+			huffman::decompress(argv[2], argv[3]);
+			return 0;
+		}
+	}
+
+	cout << "Usage:" << endl;
+	cout << "      g++ -o huffman main.cc bitstream.h huffman.h" << std::endl;
+	cout << "      huffman -c <infile> <outfile>" << endl;
+	cout << "      huffman -d <infile> <outfile>" << endl;
+	return 0;
 }
